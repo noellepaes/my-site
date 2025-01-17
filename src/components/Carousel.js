b48ae5cf05e80b80c85ae7339978aee0ad7f1d31
“noellepaes” <noellepaes12345@gmail.com>
2025-01-17 17:48:31 -0300

    tudo responsivo

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Carousel } from 'react-bootstrap';
 
 const MyCarousel = () => (
-  <Carousel>
-    <Carousel.Item>
-      <img className="d-block w-100" src="https://github.com/brediweb/estagio-frontend/blob/master/ARQUIVOS/Grupo%20610.png?raw=true" alt="First slide" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img className="d-block w-100" src="https://github.com/brediweb/estagio-frontend/blob/master/ARQUIVOS/banner2.png?raw=true" alt="Second slide" />
-    </Carousel.Item>
-  </Carousel>
+  <div className="carousel-container">
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://github.com/brediweb/estagio-frontend/blob/master/ARQUIVOS/Grupo%20610.png?raw=true"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://github.com/brediweb/estagio-frontend/blob/master/ARQUIVOS/banner2.png?raw=true"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  </div>
 );
 
 export default MyCarousel;
